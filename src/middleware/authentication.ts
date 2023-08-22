dacc17ad3cdf00fd68d8580f4c0817687a4867db
Ahmed Shalaab <ahmed.shalaab@paperworks.ae>
2023-08-22 10:25:48 +0400

    prettier refactor

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt, { Secret, VerifyErrors } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt, { Secret, VerifyErrors } from "jsonwebtoken";
 
 interface DecodedToken {
   id: string;
@@ -11,22 +11,24 @@ interface DecodedToken {
 }
 
 function authenticateToken(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Token format: "Bearer <token>"
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Token format: "Bearer <token>"
 
   if (!token) {
-    return res.status(401).json({ error: 'Unauthorized: Missing token.' });
+    return res.status(401).json({ error: "Unauthorized: Missing token." });
   }
 
   const jwtSecret: Secret | undefined = process.env.JWT_SECRET;
 
   if (!jwtSecret) {
-    return res.status(500).json({ error: 'Internal Server Error: JWT secret is missing.' });
+    return res
+      .status(500)
+      .json({ error: "Internal Server Error: JWT secret is missing." });
   }
 
   jwt.verify(token, jwtSecret, (err: VerifyErrors | null, decoded: any) => {
     if (err) {
-      return res.status(403).json({ error: 'Forbidden: Invalid token.' });
+      return res.status(403).json({ error: "Forbidden: Invalid token." });
     }
 
     // Manually assert the type of the decoded payload
