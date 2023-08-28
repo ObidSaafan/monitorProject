6ff4c94d6c3e3edaa1c74f68272a031d4425cd06
ObidSaafan <obid1082002@hotmail.com>
2023-08-28 16:54:11 +0400

    added an add user and replaced registration with update info

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import { HTTP_BAD_REQUEST } from "../constants/http_status";
 import { PrismaClient, user } from "@prisma/client";
@@ -25,34 +24,62 @@ async function login(req: any, res: any) {
   }
 }
 
-async function register(req: any, res: any) {
-  const { firstname, lastname, Email, Password } = req.body;
+async function updateinformation(req: any, res: any) {
+  const { firstname, lastname, Password } = req.body;
+  const userId = req.user?.id; // Get the user's ID from the authenticated user
+
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized: User ID is missing." });
+  }
+  //TODO: not sure if i need to remove this, its a search, but isnt it already gonna search again to update the record so what is the point here?
+  // const user = await prisma.user.findUnique({
+  //   where: {
+  //     iduser: userId,
+  //   },
+  // });
+
+  const encryptedPassword = await bcrypt.hash(Password, 10);
+
+  const newUser: user = await prisma.user.update({
+    where: { iduser: userId },
+    data: {
+      firstname: firstname,
+      lastname: lastname,
+      password: encryptedPassword,
+      firstlogin: false,
+    },
+  });
+  res.send(generateTokenResponse(newUser));
+}
+//TODO: maybe create a forgot password?
+//todo: password problem, we need to make the password secure on first login
+async function addUser(req: any, res: any) {
+  const { Email, roleid, Password } = req.body;
   const user = await prisma.user.findUnique({
     where: {
       email: Email,
     },
   });
   if (user) {
-    res.status(HTTP_BAD_REQUEST).send("User already exists, please login!");
+    res.status(HTTP_BAD_REQUEST).send("User already exists");
     return;
   }
 
   const encryptedPassword = await bcrypt.hash(Password, 10);
 
-  // TODO: add Role ID integration with invites
   const newUser: user = await prisma.user.create({
     data: {
-      firstname,
-      lastname,
+      firstname: "",
+      lastname: "",
       email: Email.toLowerCase(),
       password: encryptedPassword,
-      roleid: "2",
+      roleid: roleid,
     },
   });
   res.send(generateTokenResponse(newUser));
 }
 
 router.post("/login", asyncHandler(login));
-router.post("/register", asyncHandler(register));
-
+router.post("/register", asyncHandler(updateinformation));
+router.post("/login", asyncHandler(addUser));
 export default router;
