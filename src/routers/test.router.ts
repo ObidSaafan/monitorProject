363eb4b0cda976ec72b5faa4c9345c605eaa19a0
filly7721 <hmooodshalaab@gmail.com>
2023-09-27 15:48:49 +0400

    experimented with project reports in test router

--- src/routers/test.router.ts
+++ src/routers/test.router.ts
@@ -1,48 +1,58 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, project } from "@prisma/client";
 import express, { Router } from "express";
 import asyncHandler from "express-async-handler";
-
+import ExcelJS from "exceljs";
+import { projectExcelHeaders } from "../../types/project.headers";
+import path from "path";
 type User = {};
 const user: User[] = [];
 
 const router = Router();
 const prisma = new PrismaClient();
 
-async function getUser(req: any, res: any) {
-  //perform get
-  console.log("getUser");
-  const id = req.params.id;
-  res.send(user[id]);
-}
-async function createUser(req: any, res: any) {
-  //perform create
-  console.log("createUser");
-  const id = req.params.id;
-  res.send(user[id]);
-}
-async function updateUser(req: any, res: any) {
-  //perform update
-  console.log("updateUser");
-  const id = req.params.id;
-  res.send(user[id]);
-}
-async function deleteUser(req: any, res: any) {
-  //perform delete
-  console.log("deleteUser");
-  const id = req.params.id;
-  res.send(user[id]);
+async function exportTable(req: express.Request, res: express.Response) {
+  const projects = await prisma.project.findMany({
+    include: { client: true, pmclient: true, Sprojectmanager: true },
+  });
+
+  const formattedProjects = projects.map((proj) => {
+    return {
+      ...proj,
+      projectmanager:
+        proj.Sprojectmanager.firstname + " " + proj.Sprojectmanager.lastname,
+      clientpm: proj.pmclient.name,
+      client: proj.client.clientname,
+      projectstartdate: proj.projectstartdate.toISOString(),
+    };
+  });
+
+  let workbook = new ExcelJS.Workbook(); // Creating workbook
+  let worksheet = workbook.addWorksheet("Profile"); // Creating worksheet
+
+  worksheet.columns = projectExcelHeaders;
+
+  worksheet.addRows(formattedProjects);
+
+  const filePath = path.join(__dirname, "projects.xlsx");
+  await workbook.xlsx.writeFile(filePath);
+
+  // Set response headers
+  res.setHeader(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  );
+  res.setHeader("Content-Disposition", "attachment; filename=projects.xlsx");
+
+  // Send the Excel file as a response
+  res.sendFile(filePath, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Sent");
+    }
+  });
 }
 
-// router.get("/:id", asyncHandler(getUser));
-// router.patch("/:id", asyncHandler(updateUser));
-// router.post("/:id", asyncHandler(createUser));
-// router.delete("/:id", asyncHandler(deleteUser));
-
-router
-  .route("/:id")
-  .get(asyncHandler(getUser))
-  .post(asyncHandler(createUser))
-  .patch(asyncHandler(updateUser))
-  .delete(asyncHandler(deleteUser));
+router.get("/", exportTable);
 
 export default router;
---
+++ types/project.headers.ts
@@ -0,0 +1,105 @@
+//
+export const projectExcelHeaders = [
+  {
+    header: "project ID",
+    key: "idproject",
+    width: 10,
+  },
+  {
+    header: "Project Name",
+    key: "projectname",
+    width: 10,
+  },
+  {
+    header: "Description",
+    key: "description",
+    width: 10,
+  },
+  {
+    header: "Project Type",
+    key: "projecttype",
+    width: 10,
+  },
+  {
+    header: "Project Status",
+    key: "projectstatus",
+    width: 10,
+  },
+  {
+    header: "Project Start Date ",
+    key: "projectstartdate",
+    width: 10,
+  },
+  {
+    header: "Project Manager",
+    key: "projectmanager",
+    width: 10,
+  },
+  {
+    header: "Client Project Manager",
+    key: "clientpm",
+    width: 10,
+  },
+  {
+    header: "Duration Of Project",
+    key: "durationOfproject",
+    width: 10,
+  },
+  {
+    header: "Planned Completion Date",
+    key: "plannedcompletiondate",
+    width: 10,
+  },
+  {
+    header: "Currency",
+    key: "currency",
+    width: 10,
+  },
+  {
+    header: "Contract Value",
+    key: "contractvalue",
+    width: 10,
+  },
+  {
+    header: "Contract Status",
+    key: "contractstatus",
+    width: 10,
+  },
+  {
+    header: "Reference Number",
+    key: "referencenumber",
+    width: 10,
+  },
+  {
+    header: "Expected Profit",
+    key: "expectedprofit",
+    width: 10,
+  },
+  {
+    header: "Actual Profit",
+    key: "actualprofit",
+    width: 10,
+  },
+  {
+    header: "Client Name",
+    key: "client",
+    width: 10,
+  },
+  {
+    header: "Comments",
+    key: "comments",
+    width: 10,
+  },
+  {
+    //! add key
+    header: "Actual Spend",
+    key: "",
+    width: 10,
+  },
+  {
+    //! add key
+    header: "Total Revenue recognized",
+    key: "",
+    width: 10,
+  },
+];
